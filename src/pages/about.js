cdc0ef6e752c8f5135966ba16642fc943f40148a
Zumi Riekse <zumiriekse@hotmail.com>
2025-08-04 11:15:37 -0500

    picture and about started

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,17 +2,25 @@ import * as React from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 import { StaticImage } from 'gatsby-plugin-image'
+import * as styles from './about.module.css'
 
 const AboutPage = () => {
     return (
       
       <Layout pageTitle="about">
-          <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        <div className={styles.flexContainer}>
           <StaticImage
             alt="A headshot of Zumi Riekse"
             src="../images/zumi_profile.jpg"
+            className={styles.profileImage}
           />
+          <p className={styles.bioText}>
+            Hi there! I'm the proud creator of this site, which I built with Gatsby.
+            I like physics, code, circuits, and cats.
+        </p>
+        </div>
         </Layout>
+       
     )
   }
 
